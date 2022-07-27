ead38e174355539b4eeb3603305ecfe8330c63f9
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2022-07-27 22:12:23 +0000

    updated data

--- src/pages/Home/HomeHighlights.tsx
+++ src/pages/Home/HomeHighlights.tsx
@@ -16,11 +16,11 @@ const Wrapper = styled.div`
 export const HomeHighlights = () => {
     return (
         <Wrapper>
-            <Highlight title="Internships" data="+2" />
+            <Highlight title="Internships" data="2" />
             <Highlight title="DSA Problems Solved" data={`+${totalProblems()}`} />
             <Link to="/projects"><Highlight title="Projects" data="+10" /></Link>
-            <Highlight title="Research Papers" data="+2" />
-            <Highlight title="Certifications" data="+8" />
+            <Highlight title="Research Papers" data="2" />
+            <Highlight title="Certifications" data="+16" />
             <Highlight title="Games Developed" data="+2" />
         </Wrapper>
     );
