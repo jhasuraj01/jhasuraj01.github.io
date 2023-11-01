c6f84d7926c571eea71bd4c014f872ae8910bbfd
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2023-11-01 22:42:56 +0000

    feat: parse codeforces website for problems count

--- update/src/fetch/codeforces.ts
+++ update/src/fetch/codeforces.ts
@@ -1,3 +1,4 @@
+import { parse } from 'node-html-parser';
 import fetch from 'node-fetch';
 
 interface FetchCodeForcesReturnType {
@@ -26,26 +27,40 @@ interface CodeForcesUserStatusResponse {
 
 export const fetchCodeForces = async (): Promise<FetchCodeForcesReturnType> => {
     const infoReq = await fetch("https://codeforces.com/api/user.info?handles=jhasuraj01")
-    const statusReq = await fetch("https://codeforces.com/api/user.status?handle=jhasuraj01")
+    const profileReq = await fetch("https://codeforces.com/profile/jhasuraj01/")
+
+    const profileHTML = await profileReq.text();
+    const dom = parse(profileHTML);
+    const domElement = dom.querySelector('._UserActivityFrame_counterValue')
+    if(domElement === null)
+        throw new Error('Failed to Fetch CodeForces Profile');
+
+    const problemsCountString = domElement.innerText;
+    console.log({problemsCountString})
+    const problemsCount = Number(problemsCountString.split(' ')[0]);
+
+    if(typeof problemsCount !== "number")
+        throw new Error('Failed to Parse CodeForces Problems Count');
     
     const info = await infoReq.json() as CodeForcesUserInfoResponse;
-    const status = await statusReq.json() as CodeForcesUserStatusResponse;
+    // const status = await statusReq.json() as CodeForcesUserStatusResponse;
 
     // may include duplicates for multiple correct attempts
-    const allCorrectAttempts: (string | null)[] = status.result.map(attempt => {
-        if(attempt.verdict == 'OK') {
-            return attempt.problem.contestId + attempt.problem.index
-        }
-        else {
-            return null
-        }
-    }).filter(Boolean)
+    // const allCorrectAttempts: (string | null)[] = status.result.map(attempt => {
+    //     if(attempt.verdict == 'OK') {
+    //         return attempt.problem.contestId + attempt.problem.index
+    //     }
+    //     else {
+    //         return null
+    //     }
+    // }).filter(Boolean)
 
-    const uniqueCorrectAttempts = new Set(allCorrectAttempts);
+    // const uniqueCorrectAttempts = new Set(allCorrectAttempts);
 
     return {
         maxRating: info.result[0].maxRating,
-        problemsCount: uniqueCorrectAttempts.size,
+        // problemsCount: uniqueCorrectAttempts.size,
+        problemsCount: problemsCount,
         rank: info.result[0].maxRank
     }
 }
