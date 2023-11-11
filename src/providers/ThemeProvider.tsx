16cd9a4345af231c565cef8cf2525a8e4d992402
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-11-11 22:33:43 +0530

    feat: added theme

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,12 +1,54 @@
 import { createContext, useContext, Context,  } from 'react';
 
 export interface Theme {
-    color: string;
+    color: {
+        primary: string
+        secondary: string
+        themePrimary: string
+        themeSecondary: string
+    }
+    background: {
+        primary: string
+        secondary: string
+        themePrimary: string
+        themeSecondary: string
+    }
+    border: {
+        primary: string
+        secondary: string
+        themePrimary: string
+        themeSecondary: string
+    }
+    scroll: {
+        thumb: string
+        thumbHover: string
+    }
 }
 
 const lightTheme: Theme = {
-    color: 'black',
-}
+    color: {
+        primary: "#303030",
+        secondary: "#5A5A5A",
+        themePrimary: "#8D33FF",
+        themeSecondary: "#FFFFFF",
+    },
+    background: {
+        primary: "#FFFFFF",
+        secondary: "#B780FF",
+        themePrimary: "#F1E6FF",
+        themeSecondary: "#9A4DFF",
+    },
+    border: {
+        primary: "#303030",
+        secondary: "#5A5A5A",
+        themePrimary: "#9A4DFF",
+        themeSecondary: "#F1E6FF",
+    },
+    scroll: {
+        thumb: "#CCCCCC",
+        thumbHover: "#B3B3B3",
+    },
+};
 
 export const ThemeContext: Context<Theme> = createContext<Theme>(lightTheme);
 
