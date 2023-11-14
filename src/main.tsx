ba9a9ff199dc38c97dee84b845e8f54f6d9b1d29
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-11-14 12:50:33 +0530

    fix: scroll style not working

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import { GlobalStyle } from './GlobalStyle';
+import './main.scss';
 import { ThemeProvider } from './providers/ThemeProvider';
 import reportWebVitals from './reportWebVitals';
 
