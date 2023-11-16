76eb9ab912481b0b02188fa90d9792dc7017dcc7
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2023-11-16 11:44:57 +0530

    fix: github pages not found error (#22)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,13 @@ export default defineConfig({
         svgr(),
     ],
     build: {
+      emptyOutDir: true,
       outDir: 'build',
+      rollupOptions: {
+        input: {
+          app: resolve(__dirname, 'index.html'),
+          404: resolve(__dirname, '404.html'),
+        },
+      },
     },
 })
